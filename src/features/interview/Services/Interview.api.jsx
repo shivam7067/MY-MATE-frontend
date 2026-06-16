15fb3d0893597fe03836648e72d337ad07a9487f
Shivam Rajput <shivamrajput7067620336@gmail.com>
2026-06-16 12:10:31 +0530

    Update Interview.api.jsx

--- src/features/interview/Services/Interview.api.jsx
+++ src/features/interview/Services/Interview.api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000/api",
+    baseURL: "https://gen-backend-faau.onrender.com/api",
     withCredentials: true,
 });
 
